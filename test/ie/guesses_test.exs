f5cc9ed4fe0b8d053c2eac4d52b23f40f6f626c6
Moritz <moritz.ploss@gmail.com>
2020-02-28 17:09:16 +0100

    add test for add function of Guesses module

--- test/ie/guesses_test.exs
+++ test/ie/guesses_test.exs
@@ -20,12 +20,22 @@ defmodule IE.GuessesTest do
     assert MapSet.size(guesses_3.hits) == 2
   end
 
-  test "the add function should accept 'hits'" do
+  test "the add function should accept hits" do
     guesses = IE.Guesses.new()
     {:ok, hit_coordinate1} = IE.Coordinate.new(1, 2)
 
     guesses_with_hit = IE.Guesses.add(guesses, :hit, hit_coordinate1)
     assert MapSet.member?(guesses_with_hit.hits, hit_coordinate1)
     assert not MapSet.member?(guesses_with_hit.misses, hit_coordinate1)
+  end
+
+
+  test "the add function should accept misses" do
+    guesses = IE.Guesses.new()
+    {:ok, miss_coordinate1} = IE.Coordinate.new(1, 2)
+
+    guesses_with_misses = IE.Guesses.add(guesses, :miss, miss_coordinate1)
+    assert MapSet.member?(guesses_with_misses.misses, miss_coordinate1)
+    assert not MapSet.member?(guesses_with_misses.hits, miss_coordinate1)
   end
 end
