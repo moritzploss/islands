c84bbb8221d8c280003be1bffa26e42123ef6190
Moritz <moritz.ploss@gmail.com>
2020-03-01 18:41:48 +0100

    add position island functionality and unit tests

--- lib/ie/game.ex
+++ lib/ie/game.ex
@@ -1,15 +1,49 @@
 defmodule IE.Game do
   use GenServer
-  alias IE.{Board, Guesses, Rules}
 
-  # Client
+  alias IE.Board
+  alias IE.Coordinate
+  alias IE.Guesses
+  alias IE.Island
+  alias IE.Rules
 
+  @players [:player1, :player2]
+
+  # Client-Side
+
+  @spec start_link(binary) :: :ignore | {:error, any} | {:ok, pid}
   def start_link(player_name) when is_binary(player_name) do
     GenServer.start_link(__MODULE__, player_name, [])
   end
 
   def add_player2(game, name) when is_binary(name) do
    GenServer.call(game, {:add_player, name})
+  end
+
+  def position_island(game, player, island_type, row, col) when player in @players do
+    GenServer.call(game, {:position_island, player, island_type, row, col})
+  end
+
+  # GenServer Helper
+
+  defp update_player2_name(state, name) do
+    put_in(state.player2.name, name)
+  end
+
+  defp update_rules(state, rules) do
+    %{state | rules: rules}
+  end
+
+  defp update_board(state, player, board) do
+    Map.update!(state, player, fn player -> %{player | board: board} end)
+  end
+
+  defp reply_with_state(state, reply) do
+    {:reply, reply, state}
+  end
+
+  defp get_player_board(state, player) do
+    Map.get(state, player).board
   end
 
   # GenServer Callbacks
@@ -20,29 +54,30 @@ defmodule IE.Game do
     {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
   end
 
-  defp update_player2_name(state, name) do
-    put_in(state.player2.name, name)
-  end
-
-  defp update_rules(state, rules) do
-    %{state | rules: rules}
-  end
-
-  defp reply_success(state, reply) do
-    {:reply, reply, state}
-  end
-
   def handle_call({:add_player, name}, _from, state) do
     case Rules.check(state.rules, :add_player) do
       :error -> {:reply, :error, state}
       {:ok, rules} -> state
                       |> update_player2_name(name)
                       |> update_rules(rules)
-                      |> reply_success(:ok)
+                      |> reply_with_state(:ok)
     end
   end
 
-  # def handle_cast(msg, state) do
-
-  # end
+  def handle_call({:position_island, player, island_type, row, col}, _from, state) do
+    player_board = get_player_board(state, player)
+    with {:ok, rules} <- Rules.check(state.rules, {:position_island, player}),
+      {:ok, upper_left} <- Coordinate.new(row, col),
+      {:ok, island} <- Island.new(island_type, upper_left),
+      %{} = board <- Board.position_island(player_board, island_type, island)
+    do
+      state
+      |> update_board(player, board)
+      |> update_rules(rules)
+      |> reply_with_state(:ok)
+    else
+      :error -> {:reply, :error, state}
+      {:error, error} -> {:reply, {:error, error}, state}
+    end
+  end
 end
--- test/ie/game_test.exs
+++ test/ie/game_test.exs
@@ -19,4 +19,47 @@ defmodule IE.GameTest do
 
     assert state.player2.name == "Player 2"
   end
+
+  test "allow players to position islands" do
+    {:ok, game} = Game.start_link("Player 1")
+    :ok = Game.add_player2(game, "Player 2")
+    state = :sys.get_state(game)
+
+    assert state.rules.state == :players_set
+    :ok = Game.position_island(game, :player1, :square, 1, 1)
+    :ok = Game.position_island(game, :player2, :square, 1, 1)
+  end
+
+  test "handle unallowed actions error" do
+    {:ok, game} = Game.start_link("Player 1")
+    :error = Game.position_island(game, :player1, :dot, 1, 1)
+  end
+
+  test "handle invalid coordinates error" do
+    {:ok, game} = Game.start_link("Player 1")
+    :ok = Game.add_player2(game, "Player 2")
+
+    {:error, :invalid_coordinate} = Game.position_island(
+      game, :player1, :square, 1, 10
+    )
+  end
+
+  test "handle invalid island type error" do
+    {:ok, game} = Game.start_link("Player 1")
+    :ok = Game.add_player2(game, "Player 2")
+
+    {:error, :invalid_island_type} = Game.position_island(
+      game, :player1, :invalid_type, 1, 1
+    )
+  end
+
+  test "handle overlapping islands error" do
+    {:ok, game} = Game.start_link("Player 1")
+    :ok = Game.add_player2(game, "Player 2")
+
+    :ok = Game.position_island(game, :player1, :dot, 1, 1)
+    {:error, :overlapping_island} = Game.position_island(
+      game, :player1, :square, 1, 1
+    )
+  end
 end
