2996f7f7ffa06c2056648c945251eea8765d48fa
Moritz <moritz.ploss@gmail.com>
2020-03-01 11:44:29 +0100

    add unit test for rules

--- test/ie/rules_test.exs
+++ test/ie/rules_test.exs
@@ -42,4 +42,14 @@ defmodule IE.RulesTest do
     {:ok, player2_set} = Rules.check(player1_set, {:set_islands, :player2})
     assert player2_set.state == :player1_turn
   end
+
+  test "once in player1_turn state, disallow transitions to previous states" do
+    rules = %Rules{Rules.new() | state: :player1_turn}
+
+    :error = Rules.check(rules, :add_player)
+    :error = Rules.check(rules, {:position_islands, :player2})
+    :error = Rules.check(rules, {:position_islands, :player1})
+    :error = Rules.check(rules, {:set_islands, :player1})
+    :error = Rules.check(rules, {:set_islands, :player2})
+  end
 end
