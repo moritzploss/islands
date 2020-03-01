7b672b6c9cfa75a5a4df08145440a59e53469fed
Moritz <moritz.ploss@gmail.com>
2020-03-01 17:19:24 +0100

    add functionality to add second player to game

--- lib/ie/game.ex
+++ lib/ie/game.ex
@@ -2,9 +2,17 @@ defmodule IE.Game do
   use GenServer
   alias IE.{Board, Guesses, Rules}
 
+  # Client
+
   def start_link(player_name) when is_binary(player_name) do
     GenServer.start_link(__MODULE__, player_name, [])
   end
+
+  def add_player2(game, name) when is_binary(name) do
+   GenServer.call(game, {:add_player, name})
+  end
+
+  # GenServer Callbacks
 
   def init(player_name) do
     player1 = %{name: player_name, board: Board.new(), guesses: Guesses.new()}
@@ -12,9 +20,27 @@ defmodule IE.Game do
     {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
   end
 
-  # def handle_call(msg, from, state) do
+  defp update_player2_name(state, name) do
+    put_in(state.player2.name, name)
+  end
 
-  # end
+  defp update_rules(state, rules) do
+    %{state | rules: rules}
+  end
+
+  defp reply_success(state, reply) do
+    {:reply, reply, state}
+  end
+
+  def handle_call({:add_player, name}, _from, state) do
+    case Rules.check(state.rules, :add_player) do
+      :error -> {:reply, :error, state}
+      {:ok, rules} -> state
+                      |> update_player2_name(name)
+                      |> update_rules(rules)
+                      |> reply_success(:ok)
+    end
+  end
 
   # def handle_cast(msg, state) do
 
--- test/ie/game_test.exs
+++ test/ie/game_test.exs
@@ -3,13 +3,20 @@ defmodule IE.GameTest do
   alias IE.Game
 
   test "setup new game by starting GenServer with name of player1" do
-    {:ok, pid} = Game.start_link("Test User")
+    {:ok, pid} = Game.start_link("Player 1")
     state = :sys.get_state(pid)
-    name = Map.fetch!(state.player1, :name)
 
-    assert name == "Test User"
+    assert state.player1.name == "Player 1"
     assert Map.has_key?(state, :player1)
     assert Map.has_key?(state, :player2)
     assert Map.has_key?(state, :rules)
+  end
+
+  test "add player2 to game" do
+    {:ok, game} = Game.start_link("Player 1")
+    :ok = Game.add_player2(game, "Player 2")
+    state = :sys.get_state(game)
+
+    assert state.player2.name == "Player 2"
   end
 end
