88f29c130bf63e3418e1b0f327c29dbffff9ad16
Moritz <moritz.ploss@gmail.com>
2020-03-01 11:40:07 +0100

    simplify tests

--- test/ie/rules_test.exs
+++ test/ie/rules_test.exs
@@ -12,12 +12,6 @@ defmodule IE.RulesTest do
     :error = Rules.check(rules, :random_action)
   end
 
-  test "allow players to set their islands if not set yet" do
-    {:ok, rules} = Rules.check(Rules.new(), :add_player)
-    {:ok, _new_rules} = Rules.check(rules, {:position_island, :player1})
-    # :error = Rules.check(new_rules, {:position_island, :player1})
-  end
-
   test "players can set islands multiple times when in players_set state" do
     rules = %Rules{Rules.new() | state: :players_set}
 
