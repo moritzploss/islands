fbd2c68cc2edc7d5a61527022191acc0605f85b9
Moritz <moritz.ploss@gmail.com>
2020-03-02 20:04:31 +0100

    improve naming convention for clarity

--- test/ie/game_test.exs
+++ test/ie/game_test.exs
@@ -3,8 +3,8 @@ defmodule IE.GameTest do
   alias IE.Game
 
   test "setup new game by starting GenServer with name of player1" do
-    {:ok, pid} = Game.start_link("Player 1")
-    state = :sys.get_state(pid)
+    {:ok, game_pid} = Game.start_link("Player 1")
+    state = :sys.get_state(game_pid)
 
     assert state.player1.name == "Player 1"
     assert Map.has_key?(state, :player1)
@@ -13,53 +13,53 @@ defmodule IE.GameTest do
   end
 
   test "add player2 to game" do
-    {:ok, game} = Game.start_link("Player 1")
-    :ok = Game.add_player2(game, "Player 2")
-    state = :sys.get_state(game)
+    {:ok, game_pid} = Game.start_link("Player 1")
+    :ok = Game.add_player2(game_pid, "Player 2")
+    state = :sys.get_state(game_pid)
 
     assert state.player2.name == "Player 2"
   end
 
   test "allow players to position islands" do
-    {:ok, game} = Game.start_link("Player 1")
-    :ok = Game.add_player2(game, "Player 2")
-    state = :sys.get_state(game)
+    {:ok, game_pid} = Game.start_link("Player 1")
+    :ok = Game.add_player2(game_pid, "Player 2")
+    state = :sys.get_state(game_pid)
 
     assert state.rules.state == :players_set
-    :ok = Game.position_island(game, :player1, :square, 1, 1)
-    :ok = Game.position_island(game, :player2, :square, 1, 1)
+    :ok = Game.position_island(game_pid, :player1, :square, 1, 1)
+    :ok = Game.position_island(game_pid, :player2, :square, 1, 1)
   end
 
   test "handle unallowed actions error" do
-    {:ok, game} = Game.start_link("Player 1")
-    :error = Game.position_island(game, :player1, :dot, 1, 1)
+    {:ok, game_pid} = Game.start_link("Player 1")
+    :error = Game.position_island(game_pid, :player1, :dot, 1, 1)
   end
 
   test "handle invalid coordinates error" do
-    {:ok, game} = Game.start_link("Player 1")
-    :ok = Game.add_player2(game, "Player 2")
+    {:ok, game_pid} = Game.start_link("Player 1")
+    :ok = Game.add_player2(game_pid, "Player 2")
 
     {:error, :invalid_coordinate} = Game.position_island(
-      game, :player1, :square, 1, 10
+      game_pid, :player1, :square, 1, 10
     )
   end
 
   test "handle invalid island type error" do
-    {:ok, game} = Game.start_link("Player 1")
-    :ok = Game.add_player2(game, "Player 2")
+    {:ok, game_pid} = Game.start_link("Player 1")
+    :ok = Game.add_player2(game_pid, "Player 2")
 
     {:error, :invalid_island_type} = Game.position_island(
-      game, :player1, :invalid_type, 1, 1
+      game_pid, :player1, :invalid_type, 1, 1
     )
   end
 
   test "handle overlapping islands error" do
-    {:ok, game} = Game.start_link("Player 1")
-    :ok = Game.add_player2(game, "Player 2")
+    {:ok, game_pid} = Game.start_link("Player 1")
+    :ok = Game.add_player2(game_pid, "Player 2")
 
-    :ok = Game.position_island(game, :player1, :dot, 1, 1)
+    :ok = Game.position_island(game_pid, :player1, :dot, 1, 1)
     {:error, :overlapping_island} = Game.position_island(
-      game, :player1, :square, 1, 1
+      game_pid, :player1, :square, 1, 1
     )
   end
 end
