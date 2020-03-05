eb10a181f4dc572cfde6a94a47663b8eefcce732
Moritz <moritz.ploss@gmail.com>
2020-03-05 21:59:28 +0100

    add unit tests for GameSupervisor

--- test/ie/game_supervisor_test.exs
+++ test/ie/game_supervisor_test.exs
@@ -6,7 +6,19 @@ defmodule IE.GameSupervisorTest do
   test "start and stop a supervised game process" do
     {:ok, pid} = GameSupervisor.start_game("Player 1")
     assert Process.alive?(pid)
+
     GameSupervisor.stop_game("Player 1")
     assert not Process.alive?(pid)
+  end
+
+  test "allow pid access by player name via Registry" do
+    {:ok, pid} = GameSupervisor.start_game("Player 1")
+    via = IE.Game.via_tuple("Player 1")
+    assert GenServer.whereis(via) == pid
+
+    state = :sys.get_state(via)
+    assert state.player1.name == "Player 1"
+
+    GameSupervisor.stop_game("Player 1")
   end
 end
